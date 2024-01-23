c30f5ddf987da291a790195cedbbe834f403910f
HKM-Rusaik <hkmrusaik@gmail.com>
2024-01-23 16:00:17 +0530

    Handled Exceptions

--- Day2_Tip Calculator/main.py
+++ Day2_Tip Calculator/main.py
@@ -1,11 +1,25 @@
 print("Welcome to the tip calculator.")
-totalBillAmount = input("What was the total bill? ")
-tipPercentage = input("What percentage tip would you like to give? 10, 12, or 15? ")
-noOfBillOwners = input("How many people to split the bill? ")
+try:
+    totalBillAmount = input("What was the total bill? ")
+    tipPercentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+    if tipPercentage not in [10, 12, 15]:
+        raise ValueError("Invalid input. Please enter 10, 12, or 15.")
+    noOfBillOwners = input("How many people to split the bill? ")
 
-totalAmountToPay = int(totalBillAmount) + int(totalBillAmount) * (int(tipPercentage)/100)
+except ValueError as ve:
+    print(ve)
+    print("Please enter valid input.")
 
-amountToEachPersonToPay = int(totalAmountToPay)/ int(noOfBillOwners)
+try:
+    totalAmountToPay = int(totalBillAmount) + int(totalBillAmount) * (tipPercentage / 100)
 
-print(f"Each person should pay : ${amountToEachPersonToPay}")
+    amountToEachPersonToPay = totalAmountToPay / int(noOfBillOwners)
 
+    print(f"Each person should pay: ${amountToEachPersonToPay:.2f}")
+
+except ZeroDivisionError:
+    print("Number of bill owners cannot be zero.")
+except ValueError:
+    print("Invalid input. Please make sure to enter valid numbers.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
